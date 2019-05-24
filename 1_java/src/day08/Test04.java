0b4bf426a78b4cbc9158b9839324db95a87d54e1
student <student@M15015>
2019-05-24 11:21:26 +0900

    자바 기초 학습 COMMIT

---
+++ 1_java/src/day08/Test04.java
@@ -0,0 +1,18 @@
+package day08;
+
+public class Test04 {
+	public static void main(String[] args) {
+		Circle  c = new Circle();
+		Rectangle r = new Rectangle();
+		
+		/*
+		 * Drawable d =r; d.draw(); d =c; d.draw();
+		 * 
+		 * 
+		 * Movable m =r; m.move();; m =c; m.move();
+		 * 
+		 * MovableAndDrawable[] md = {c,r}; md[0].draw(); md[0].move(); md[1].draw();
+		 * md[1].move();
+		 */
+	}
+}
